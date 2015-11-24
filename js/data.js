a8649a3ad381f8d41f24b2e1490f358d7a29d3c4
luvinhthinh <luvinhthinh.mono@gmail.com>
2015-11-24 12:31:12 +1100

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,18 @@
+
+// data is an array of progress bar information
+// max is the upper bound of progress bar
+// completeColor : color of part of progress bar which illustrates completed area
+// incompleteColor : color of part of progress bar which illustrates incompleted area
+
+data = [
+  {
+    max : 100,
+    completeColor : 'red',
+    incompleteColor : 'blue'
+  },
+  {
+    max : 110,
+    completeColor : 'yellow',
+    incompleteColor : 'blue'
+  }
+];
